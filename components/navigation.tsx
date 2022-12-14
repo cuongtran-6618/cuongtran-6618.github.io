4f961557fae8d3e64a9aeed83888d0916b1be3c8
Cuong Tran <mailtocuong@gmail.com>
2022-12-15 00:07:26 +0200

    minor style fix

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -6,13 +6,15 @@ export default function Navigation() {
   return (
     <>
       <div className='flex flex-row justify-center'>
+        <Link href='/' title='homepage'>
           <Image src={LogoSrc} alt='logo' height={200} width={200} className="items-center"></Image>
+        </Link>
       </div>  
       <div className='flex flex-row justify-between items-baseline flex-wrap m-3'>          
-        <div className='hover:underline'><Link href='/services'>Services</Link></div>
-        <div className='hover:underline'><Link href='/our-work'>Our work</Link></div>
-        <div className='hover:underline'><Link href='/about-us'>About us</Link></div>
-        <div className='hover:underline'><Link href='/contact-us'>Contact us</Link></div>                 
+        <div className='hover:underline underline-offset-8'><Link href='/services'>Services</Link></div>
+        <div className='hover:underline underline-offset-8'><Link href='/our-work'>Our work</Link></div>
+        <div className='hover:underline underline-offset-8'><Link href='/about-us'>About us</Link></div>
+        <div className='hover:underline underline-offset-8'><Link href='/contact-us'>Contact us</Link></div>                 
       </div>
     </>
   )
