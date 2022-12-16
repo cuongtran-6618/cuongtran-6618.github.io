1686e2e763d7fccc85ec822634c2acb477cf42c9
Cuong Tran <mailtocuong@gmail.com>
2022-12-16 21:53:47 +0200

    add loop and auto switch for slider

--- components/slider.tsx
+++ components/slider.tsx
@@ -1,9 +1,43 @@
-import React from "react"
-import { useKeenSlider } from "keen-slider/react"
-import "keen-slider/keen-slider.min.css"
+import React from "react";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
 
 export default function Slider() {
-  const [sliderRef] = useKeenSlider()
+    const [sliderRef] = useKeenSlider(
+        {
+            loop: true,
+        },
+        [
+            (slider) => {
+                let timeout
+                let mouseOver = false
+                function clearNextTimeout() {
+                    clearTimeout(timeout)
+                }
+                function nextTimeout() {
+                    clearTimeout(timeout)
+                    if (mouseOver) return
+                    timeout = setTimeout(() => {
+                        slider.next()
+                    }, 3000)
+                }
+                slider.on("created", () => {
+                    slider.container.addEventListener("mouseover", () => {
+                        mouseOver = true
+                        clearNextTimeout()
+                    })
+                    slider.container.addEventListener("mouseout", () => {
+                        mouseOver = false
+                        nextTimeout()
+                    })
+                    nextTimeout()
+                })
+                slider.on("dragStarted", clearNextTimeout)
+                slider.on("animationEnded", nextTimeout)
+                slider.on("updated", nextTimeout)
+            },
+        ]
+    );
 
   return (
     <div ref={sliderRef} className="keen-slider">
