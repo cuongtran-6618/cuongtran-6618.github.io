7727dc7cb8d0861bd5e89def5423dad1f75a6385
Cuong Tran <mailtocuong@gmail.com>
2023-01-07 21:28:05 +0200

    change to use rubik google font preload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
+import next from "next";
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
+import { Rubik } from "@next/font/google";
+
+const rubik = Rubik({ subsets: ["latin"], weight :["400", "500", "700"] });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} className="font-serif" />;
+  return (
+    <main className={rubik.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
 export default MyApp;
