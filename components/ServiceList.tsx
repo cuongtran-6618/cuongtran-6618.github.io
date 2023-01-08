d9378890bd131ac142caa0189e8660b33563d63e
Cuong Tran <mailtocuong@gmail.com>
2023-01-08 13:13:23 +0200

    style fix

--- components/ServiceList.tsx
+++ components/ServiceList.tsx
@@ -3,7 +3,7 @@ import data from "../data/services.json";
 
 export default function ServiceList() {
   return (
-    <section className="lg:container lg:mx-auto pt-4 pb-4">
+    <section className="container mx-auto pt-4 pb-4">
       <div>
         <h3 className="font-bold text-4xl mb-[72px] text-white">Our Services</h3>
       </div>
