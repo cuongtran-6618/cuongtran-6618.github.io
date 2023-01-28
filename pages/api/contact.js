49f377021ff6aae4136be483e7ffeba1ce4ae477
Cuong Tran <mailtocuong@gmail.com>
2023-01-28 23:50:28 +0200

    test mail

---
+++ pages/api/contact.js
@@ -0,0 +1,22 @@
+const sgMail = require("@sendgrid/mail");
+import { loadEnvConfig } from "@next/env";
+
+export default function handler(req, res) {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: "mailtocuong@gmail.com", // Change to your recipient
+    from: req.body.email, // Change to your verified sender
+    subject: "Sending with SendGrid is Fun",
+    text: req.body.message,
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+      res.send(200);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,12 +1,83 @@
 import BackHome from "../components/BackHome";
 import Layout from "../components/Layout";
+import React from "react";
 
 export default function ContactUs() {
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const data = new FormData(e.currentTarget);
+    try {
+      const response = await fetch("/api/contact", {
+        method: "post",
+        body: new URLSearchParams(data),
+      });
+      if (!response.ok) {
+        throw new Error(`Invalid response: ${response.status}`);
+      }
+      alert("Thanks for contacting us, we will get back to you soon!");
+    } catch (err) {
+      console.error(err);
+      //alert("We can't submit the form, try again later?");
+    }
+  }
+
   return (
     <Layout>
-      <h1>Contact us</h1>
-      <div>Contact form!</div>
-      <BackHome />
+      <div className="mx-auto container">
+        <form className="container mx-auto" onSubmit={handleSubmit}>
+          <h1 className="text-center">Contact us</h1>
+          <div className="name flex flex-row justify-between mb-8">
+            <div className="mb-4 flex flex-col">
+              <label htmlFor="frm-first">First Name</label>
+              <input
+                id="frm-first"
+                type="text"
+                name="first"
+                autoComplete="given-name"
+                required
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="frm-last">Last Name</label>
+              <input
+                id="frm-last"
+                type="text"
+                name="last"
+                autoComplete="family-name"
+                required
+              />
+            </div>
+          </div>
+          <div className="email flex flex-col mb-8">
+            <label htmlFor="frm-email">Email</label>
+            <input
+              id="frm-email"
+              type="email"
+              name="email"
+              autoComplete="email"
+              required
+            />
+          </div>
+          <div className="phone flex flex-col mb-8">
+            <label htmlFor="frm-phone">Phone</label>
+            <input
+              id="frm-phone"
+              type="text"
+              name="phone"
+              autoComplete="tel"
+              required
+            />
+          </div>
+
+          <div className="message flex flex-col mb-8">
+            <label htmlFor="frm-message">Message</label>
+            <textarea id="frm-message" name="message" rows={6}></textarea>
+          </div>
+          <div className="button primary flex flex-col items-center">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
     </Layout>
   );
 }
