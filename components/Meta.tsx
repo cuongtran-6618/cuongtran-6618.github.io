d9c566ac9487b51168e766501ff13c86857e1d8b
Cuong Tran <mailtocuong@gmail.com>
2023-02-05 21:29:36 +0200

    add seo content

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -1,13 +1,13 @@
 import Head from "next/head";
+import seo from "../data/seo.json";
 
 // class generate the meta data for the web and also where to include icon etc...
 export default function Meta() {
   return (
     <Head>
-      <meta
-        name="description"
-        content={`A meta tag for Felia Consultance Oy.`}
-      />
+      <title>{seo.meta.title}</title>
+      <meta name="keywords" content={seo.meta.keywords} />
+      <meta name="description" content={seo.meta.description} />
       <meta name="robots" content="noindex,nofollow" />
       <meta name="googlebot" content="noindex,nofollow" />
     </Head>
