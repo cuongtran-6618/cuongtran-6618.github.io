96319dd523fa06e0d2f4d34a9caecd29dc282be7
Cuong Tran <mailtocuong@gmail.com>
2023-02-09 23:06:31 +0200

    footer style fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,15 +4,28 @@ import company from "../data/company.json";
 export default function BackHome() {
   return (
     <div className="text-center mt-12 text-white bg-black p-4 lg:p-20">
-      <div className="container mt-8">
+      <div className="container mt-8 lg:max-w-5xl">
         <div className="mb-8">
           <SectionHeader title={"Contact"} />
-          <div className="">
-            {company.info.address}            
+          <div className="flex flex-col mb-12 gap-12 md:justify-center md:aligns-baseline md:flex-row md:gap-4 md:gap-8">
+            <div className="p-4 lg:px-12 text-white w-full md:w-1/2 lg:w-1/3 text-left lg:text-center">
+              <div className="">
+                {company.info.address}            
+              </div>
+              <div>
+                {company.info.telephone}
+              </div>
+            </div>
+            <div className="p-4 lg:px-12 text-white w-full md:w-1/2 lg:w-1/3 text-left lg:text-center">
+              <div className="">
+                Linkedin            
+              </div>
+              <div>
+                Facebook
+              </div>
+            </div>
           </div>
-          <div>
-            {company.info.telephone}
-          </div>
+          
         </div>
         
         <div className="mb-12">© 2023 {company.info.name} Oy</div>
