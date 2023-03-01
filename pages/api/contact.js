9fba7d8f53400838487d541e953df538086ffe8c
Cuong Tran <mailtocuong@gmail.com>
2023-03-01 23:07:36 +0200

    update robot and sitemap

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -16,12 +16,12 @@ export default function handler(req, res) {
     secure: true,
   });
 
-  let $html = `<div>${req.body.message} </div><p>Sent from email: ${req.body.email}</p><p>Sent from telphone: ${req.body.phone}</p>`;
+  let $html = `<div>${req.body.message}</div><p>Sent from email: ${req.body.email}</p><p>Sent from telphone: ${req.body.phone}</p>`;
   console.log($html);
   const mailData = {
     from: process.env.SEND_MAIL_PROVIDER_USERNAME,
     to: "hello@felia.fi",
-    subject: `Message From ${req.body.name}`,
+    subject: `A new contact message from ${req.body.name}`,
     text: `${req.body.message} | Sent from: ${req.body.message}  | tel: ${req.body.phone}`,
     html: $html,
   };
